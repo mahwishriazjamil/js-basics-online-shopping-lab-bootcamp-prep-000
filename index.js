fd1af3cebc1dc0dfdac7b8ac1a6186630f470df8
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-07 01:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  var total = total()
+  var tot = total()
   cart.length = 0;
   if (cardNumber === undefined){
     return `Sorry, we don't have a credit card on file for you.`
