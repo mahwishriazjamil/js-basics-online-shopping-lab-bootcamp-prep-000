8342137b60f5d81d103617b645e410cb5aa8584d
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-07 01:39:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,8 @@ function removeFromCart(item) {
     
     for (let i = 0; i < cart.length; i++){
       if (cart[i].itemName === name){
-        
+        count = 1;
+        item
       }
     }
   
